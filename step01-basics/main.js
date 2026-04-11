e7b4cb91ef13d69c52e7a4dcbc9a82e4f5981391
tkhy2651 <tah331925@gmail.com>
2026-04-11 21:52:37 +0900

    code

---
+++ step01-basics/main.js
@@ -0,0 +1,9 @@
+const myName = "ひろや";
+const myAge = 20;
+const isStudent = true;
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+console.log(typeof myName);
+console.log(typeof myAge);
