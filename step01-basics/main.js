7fbe52663a5c3b7552e0617020fb8ad6fac663f3
tkhy2651 <tah331925@gmail.com>
2026-04-11 22:00:05 +0900

    js

--- step01-basics/main.js
+++ step01-basics/main.js
@@ -1,9 +1,12 @@
-const myName = "ひろや";
-const myAge = 20;
-const isStudent = true;
+const score = 85;
 
-console.log(myName);
-console.log(myAge);
-console.log(isStudent);
-console.log(typeof myName);
-console.log(typeof myAge);
+if (score >= 90) {
+  console.log("優");
+} else if (score >= 70) {
+  console.log("良");
+} else {
+  console.log("可");
+}
+
+const result = score >= 70 ? "合格" : "不合格";
+console.log(result);
