64a38dc5bc67aa4789cc2adfc03ee28f4e50b369
tkhy2651 <tah331925@gmail.com>
2026-04-12 14:35:57 +0900

    find

--- step03-array-object/main.js
+++ step03-array-object/main.js
@@ -16,3 +16,15 @@ const filter = document.querySelector("#filter");
 filter.textContent = evens
 
 
+// find
+const users = [
+    { name: "alice", age: 17 },
+    { name: "bob", age: 23 },
+    { name: "charlie", age: 15 },
+];
+const evenfind = users.find(function(num) {
+    return num.age >= 20;
+});
+const fid = document.querySelector("#find");
+fid.textContent = evenfind.name + " " + evenfind.age; // → "bob 23"
+
