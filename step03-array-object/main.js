9f0906c1bb1edf0ada8189e3cb580e31fe5a01a4
tkhy2651 <tah331925@gmail.com>
2026-04-12 14:27:13 +0900

    filter

--- step03-array-object/main.js
+++ step03-array-object/main.js
@@ -4,4 +4,15 @@ const doubled = names.map(function (num) {
     return num.toUpperCase()
 })
 const p = document.querySelector("p");
-p.textContent=doubled
+p.textContent = doubled
+
+
+// filter
+const numbers = [3, 7, 12, 5, 20, 8];
+const evens = numbers.filter(function(num) {
+    return num >= 10;
+});
+const filter = document.querySelector("#filter");
+filter.textContent = evens
+
+
