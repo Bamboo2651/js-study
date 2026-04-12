da7fc06b9b7d9087071ed0751134d2d22c939123
tkhy2651 <tah331925@gmail.com>
2026-04-12 14:21:37 +0900

    map

---
+++ step03-array-object/main.js
@@ -0,0 +1,6 @@
+const names = ["alice", "bob", "charlie"];
+const doubled = names.map(function (num) {
+    return num.toUpperCase()
+})
+const p = document.querySelector("p");
+p.textContent=doubled
