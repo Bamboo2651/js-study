75f4af20c2631019d090b45f8274368bcc08610e
tkhy2651 <tah331925@gmail.com>
2026-04-14 20:56:39 +0900

    json

--- step05-fetch/main.js
+++ step05-fetch/main.js
@@ -1,6 +1,7 @@
 async function main() {
     console.log("取得中---");
     const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    console.log(response)
+    const data = await response.json();
+    console.log(data)
 }
 main();
