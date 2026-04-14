d9d263dc4ce76bcb8e1cd58c4780b16348090447
tkhy2651 <tah331925@gmail.com>
2026-04-14 21:01:23 +0900

    up

--- step05-fetch/main.js
+++ step05-fetch/main.js
@@ -1,7 +1,14 @@
 async function main() {
-    console.log("取得中---");
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    const data = await response.json();
-    console.log(data)
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const data = await response.json();
+
+        document.getElementById("title").textContent = data.title;
+        document.getElementById("status").textContent = data.completed ? "完了" : "未完了";
+    } catch (error) {
+        document.getElementById("title").textContent = "取得に失敗しました";
+        console.log("エラー：", error);
+    }
 }
+
 main();
