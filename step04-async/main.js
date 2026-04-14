1a5ba6d89319b465963f75d9623efd85d8f0f644
tkhy2651 <tah331925@gmail.com>
2026-04-14 20:24:48 +0900

    async/await

--- step04-async/main.js
+++ step04-async/main.js
@@ -14,16 +14,34 @@
 // console.log(result); // 何が表示される？
 
 
+// function データを取得する() {
+//   return new Promise(function(resolve) {
+//     setTimeout(function() {
+//       resolve("取得したデータ"); // 「終わったよ！」と通知
+//     }, 1000);
+//   });
+// }
+
+// データを取得する().then(function(result) {
+//   console.log(result); // 終わったら呼ばれる
+// });
+
+// console.log("取得中...");
+
+
+
 function データを取得する() {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
-      resolve("取得したデータ"); // 「終わったよ！」と通知
-    }, 1000);
-  });
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve("取得したデータ");
+        }, 1000);
+    });
 }
 
-データを取得する().then(function(result) {
-  console.log(result); // 終わったら呼ばれる
-});
+async function main() {
+    console.log("取得中...");
+    const result = await データを取得する();
+    console.log(result);
+}
 
-console.log("取得中...");
+main();
