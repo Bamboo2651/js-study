7c6f26def67761be1e475743e93df2a724c2f10c
tkhy2651 <tah331925@gmail.com>
2026-04-16 14:19:05 +0900

    天気データ取得

--- mini02-weather-app/src/main.js
+++ mini02-weather-app/src/main.js
@@ -3,11 +3,20 @@ const button = document.getElementById('searchBtn');
 
 button.addEventListener('click', async () => {
     const city = input.value;
-    console.log('検索する都市:', city);
 
+    //都市名 → 緯度・経度
     const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=ja`;
     const geoRes = await fetch(geoUrl);
     const geoData = await geoRes.json();
+    console.log(geoData);
+    const lat = geoData.results[0].latitude;
+    const lon = geoData.results[0].longitude;
+    console.log('緯度:', lat, '経度:', lon);
 
-    console.log(geoData)
+    //緯度・経度 → 天気データ
+    const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weathercode,windspeed_10m`;
+    const weatherRes = await fetch(weatherUrl);
+    const weatherData = await weatherRes.json();
+
+    console.log(weatherData);
 });
