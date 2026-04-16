844ac4aaad892258dffba80ec8808e3f6a3c8cca
tkhy2651 <tah331925@gmail.com>
2026-04-16 14:15:49 +0900

    緯度経度取得

--- mini02-weather-app/src/main.js
+++ mini02-weather-app/src/main.js
@@ -1,7 +1,13 @@
 const input = document.getElementById('cityInput');
 const button = document.getElementById('searchBtn');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
     const city = input.value;
     console.log('検索する都市:', city);
+
+    const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=ja`;
+    const geoRes = await fetch(geoUrl);
+    const geoData = await geoRes.json();
+
+    console.log(geoData)
 });
